2a8b0fad3f8ab8e3f25b611d73afbb7c823dab0a
Gustafsson Elin (Ext) <ext.elin.gustafsson@tieto.com>
2018-11-22 15:34:31 +0100

    add operators to clickNumber, write sudo-code,regex, string to number

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -66,12 +66,90 @@ export default class Main extends Component {
         number: [...this.state.number, 0]
       }));
     }
+    if (e.target.className === 'addition') {
+      this.setState(state => ({
+        number: [...this.state.number, '+']
+      }));
+    }
+    if (e.target.className === 'multiplication') {
+      this.setState(state => ({
+        number: [...this.state.number, 'x']
+      }));
+    }
+    if (e.target.className === 'subtraction') {
+      this.setState(state => ({
+        number: [...this.state.number, '-']
+      }));
+    }
+    if (e.target.className === 'division') {
+      this.setState(state => ({
+        number: [...this.state.number, '%']
+      }));
+    }
+    if (e.target.className === 'dot') {
+      this.setState(state => ({
+        number: [...this.state.number, '.']
+      }));
+    }
   }
 
   clear = () => {
+    console.log('clear array');
     this.setState(state => ({
       number: [],
     }));
+  }
+
+  equalTo = (e) => {
+    /*Ta arrayen och gör till en string och spara i ny variabel*/
+      var stringArray = this.state.number.toString();
+      console.log(stringArray);
+    /*Ta stringen och ta bort alla ,(komma) och ersätt med ''(inget) och spara i ny variabel*/
+      var newStringArray = stringArray.replace(/,/g, '');
+      console.log(newStringArray);
+      console.log("detta är en ", typeof(newStringArray));
+    /*Ta +%-x uttryck och spara i en ny string variabel*/
+    /*Gör så jag bara behöver skriva regex istället för hela uttrycket*/
+      var regex = /[+%\-x]/g;
+      var checkOperator = newStringArray.match(regex);
+      console.log("operator ",checkOperator);
+    /*Gör stringen till en array*/
+      var slicedArray = newStringArray.split(regex);
+      console.log("detta är en ", typeof(slicedArray));
+      console.log(slicedArray);
+    /*Ta items ifrån array och gör till nummer*/
+      var numberArray = slicedArray.map(element => Number(element));
+      console.log(numberArray);
+      /*
+        gör samma sak som map
+          var x1 = slicedArray[0];
+          var x2 = slicedArray[1];
+
+          var a = Number(x1)
+          console.log(a);
+          var b = Number(x2);
+          console.log(b);
+      */
+
+        /*
+        Case
+          input: [22, -, 66, +, 88, -, 77]
+          operators =  ["-", "+", "-"]
+          numbers =   [22, 66, 88, 77]
+
+          SUDO
+          Funktion som kollar vilken operator som finns i arrayen.
+          Berätta vad varje operator ska göra.
+          Returnera resultat.
+
+          Ta operators[0] och placera mellan numbers[0] & numbers[1]. Spara resultat i variabel.
+
+          Gör en loop som går igenom operators och placera mellan result och numbers[x+x]. Uppdatera resultat variabel.
+          Ta operator[1] och placera mellan resultat och numbers[2]. Uppdatera resultat variabel.
+          Ta operator[2] och placera mellan resultat och numbers[3]. Uppdatera resultat variabel.
+        */
+
+        
   }
 
   render() {
@@ -85,17 +163,36 @@ export default class Main extends Component {
               onClick={this.clear}
               className='clear'> Clear
             </button>
-            <button style={style.division}> % </button>
+            <button
+              style={style.division}
+              onClick={this.clickNumber}
+              className='division'> %
+            </button>
           </div>
           <div style={style.buttonRow}>
             <div style={style.buttonRowOne}>
               <Numbers sendProp={this.clickNumber} />
             </div>
             <div style={style.buttonRowTwo}>
-              <button style={style.buttonOperators}>x</button>
-              <button style={style.buttonOperators}>-</button>
-              <button style={style.buttonOperators}>+</button>
-              <button style={style.buttonOperators}>=</button>
+              <button
+                style={style.buttonOperators}
+                onClick={this.clickNumber}
+                className='multiplication'> x
+              </button>
+              <button
+                style={style.buttonOperators}
+                onClick={this.clickNumber}
+                className='subtraction'> -
+              </button>
+              <button
+                style={style.buttonOperators}
+                onClick={this.clickNumber}
+                className='addition'> +
+              </button>
+              <button
+                style={style.buttonOperators}
+                onClick={this.equalTo}> =
+              </button>
             </div>
           </div>
           <div style={style.buttonRowThree}>
@@ -104,7 +201,11 @@ export default class Main extends Component {
               onClick={this.clickNumber}
               className='zero'> 0
             </button>
-            <button style={style.button}>.</button>
+            <button
+              style={style.button}
+              onClick={this.clickNumber}
+              className='dot'> .
+            </button>
           </div>
         </div>
       </div>
